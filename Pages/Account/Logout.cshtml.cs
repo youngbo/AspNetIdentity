e41b9284fc44fa9dbb6e4b0b5ccfe09a574bce0b
youngbo choi <youngbosyd@hotmail.com>
2022-10-10 11:37:51 +0900

    worked on login partial page

---
+++ Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetIdentity.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext
+                .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/index");
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetIdentity.Pages
 {
-    [Authorize]
+    //[Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;

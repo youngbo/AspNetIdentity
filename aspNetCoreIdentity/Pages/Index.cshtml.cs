995f8325fe2c80e3c2f2bab8ac827960b13a4124
youngbo choi <youngbosyd@hotmail.com>
2022-10-15 14:09:18 +0900

    removed ssl

--- aspNetCoreIdentity/Pages/Index.cshtml.cs
+++ aspNetCoreIdentity/Pages/Index.cshtml.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace aspNetCoreIdentity.Pages
 {
-    [Authorize()]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
